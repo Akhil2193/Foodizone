cfc9b7a8ff38cffc8a60a7e49288fdf10cffe9d2
Akhil2193 <bansalakhil19@gmail.com>
2021-07-29 13:14:39 +0530

    create navbar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
         <div>
             <Navbar />
-            hello world
+            
         </div>
     );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import SearchIcon from '@material-ui/icons/Search';
 function Navbar() {
     return (<nav className="navbar">
         <ul className="navbar-list">
-            <li className="logo">foodizone</li>
-            <li className="search"><input name="search-bar" id="search-bar" /><label htmlFor="search-bar"><SearchIcon /></label></li>
-            <li className="authorisation">Log in</li>
-            <li className="authorisation">Sign up</li>
+            <li className="logo navbar-list-items">foodizone</li>
+            <li className="search navbar-list-items"><input name="search-bar" id="search-bar" /><label htmlFor="search-bar"><SearchIcon style={{color:'#787878'}} fontSize="large" /></label></li>
+            <li className="authorisation navbar-list-items"><a href="#">Log in</a></li>
+            <li className="authorisation navbar-list-items"><a href="#">Sign up</a></li>
         </ul>
     </nav>)
 }
