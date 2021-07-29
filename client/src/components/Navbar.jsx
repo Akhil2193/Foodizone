fdfdeb404f4502e881946f42e8b997d5f6a36fb0
Akhil2193 <bansalakhil19@gmail.com>
2021-07-29 14:05:03 +0530

    format navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,10 @@ function Navbar() {
     return (<nav className="navbar">
         <ul className="navbar-list">
             <li className="logo navbar-list-items">foodizone</li>
-            <li className="search navbar-list-items"><input name="search-bar" id="search-bar" /><label htmlFor="search-bar"><SearchIcon style={{color:'#787878'}} fontSize="large" /></label></li>
+            <li className="search navbar-list-items">
+                <input name="search-bar" id="search-bar" placeholder="Search for restaurant, cuisine or a dish" spellCheck="false" />
+                <label htmlFor="search-bar"><SearchIcon style={{ color: '#787878' }} fontSize="large" /></label>
+            </li>
             <li className="authorisation navbar-list-items"><a href="#">Log in</a></li>
             <li className="authorisation navbar-list-items"><a href="#">Sign up</a></li>
         </ul>
