098b2d7ed87112a1217bfbb09680a9a17edb906c
Akhil2193 <bansalakhil19@gmail.com>
2021-07-31 15:08:55 +0530

    add responsiveness to the site

--- client/src/components/Restaurant.jsx
+++ client/src/components/Restaurant.jsx
@@ -13,7 +13,7 @@ function add3Dots(string, limit)
 function Restaurant(props){
     return(
         <div className="restaurant-container">
-            <img  src="https://picsum.photos/200/100" className="restaurant-image" alt="" />
+            <img  src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" className="restaurant-image" alt="" />
             {/* src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" */}
             <div className="restaurant-container-title">
                 {props.name}
