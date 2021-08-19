0a7eda783c6aa3487695c34688234f4fa386d9e4
Akhil2193 <bansalakhil19@gmail.com>
2021-08-19 17:24:52 +0530

    env path add

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require('dotenv').config({ path: './.env' })
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -12,12 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 const uri = process.env.DB_URI;
 
 app.use(session({
-    secret:process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }))
@@ -42,14 +42,14 @@ const restaurantSchema = new mongoose.Schema({
     ]
 })
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String,
-    name:String,
-    contact:String
+    email: String,
+    password: String,
+    name: String,
+    contact: String
 })
 userSchema.plugin(passportLocalMongoose);
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
-const User = new mongoose.model('User',userSchema);
+const User = new mongoose.model('User', userSchema);
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
@@ -57,9 +57,9 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(
     cors({
-        origin:'http://localhost:3000',
-        methods:'GET,HEAD,PUT,POST,PATCH,DELETE',
-        credentials:true
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,POST,PATCH,DELETE',
+        credentials: true
     })
 )
 
@@ -87,61 +87,61 @@ app.route('/api/:id')
         })
     });
 app.route('/api/search/:search')
-.get(function(req,res){
+    .get(function (req, res) {
 
-    
-    const query= {
-        $or: [
-            {name:{$regex: req.params.search} },
-            {type:{$regex: req.params.search} },
-            {foodItems:{ $elemMatch: {name :{$regex: req.params.search}}}},
-            {foodItems:{ $elemMatch: {category :{$regex: req.params.search}}}}
-            
-        ]
-    };
-    
-    Restaurant.find(query,function (err, restaurants) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.send(restaurants);
-        }
+
+        const query = {
+            $or: [
+                { name: { $regex: req.params.search } },
+                { type: { $regex: req.params.search } },
+                { foodItems: { $elemMatch: { name: { $regex: req.params.search } } } },
+                { foodItems: { $elemMatch: { category: { $regex: req.params.search } } } }
+
+            ]
+        };
+
+        Restaurant.find(query, function (err, restaurants) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.send(restaurants);
+            }
+        })
     })
-})
 //admin portal
 app.route('/admin')
     .get(function (req, res) {
         res.render('index')
     });
 
-app.post('/register',function(req,res){
+app.post('/register', function (req, res) {
     User.register({
-        username:req.body.username,
-        name:req.body.name,
-        contact:req.body.contact
-    },req.body.password,function(err,user){
-        if(err){
+        username: req.body.username,
+        name: req.body.name,
+        contact: req.body.contact
+    }, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             res.redirect('http://localhost:3000/authenticate/register')
         } else {
-            passport.authenticate('local')(req,res,function(){
+            passport.authenticate('local')(req, res, function () {
                 res.redirect('http://localhost:3000')
-                
+
             })
         }
     })
 })
-app.post('/login',function(req,res){
+app.post('/login', function (req, res) {
     const user = new User({
-        username:req.body.username,
+        username: req.body.username,
         password: req.body.password
     })
-    req.login(user,function(err){
-        if(err){
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
         } else (
-            passport.authenticate('local')(req,res,function(){
+            passport.authenticate('local')(req, res, function () {
                 var data = {
                     authentication: true
                 }
@@ -151,22 +151,22 @@ app.post('/login',function(req,res){
         )
     })
 })
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
     req.logout();
     var data = {
         authentication: true
     }
     res.json(data)
-  });
-app.get('/authenticate',function(req,res){
+});
+app.get('/authenticate', function (req, res) {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         var data = {
             authentication: true
         }
         res.json(data)
     }
-    else{
+    else {
         var data = {
             authentication: false
         }
